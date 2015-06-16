c35dedd12b3144b32d558d7e023e3a437f34d10f
rtand <rtac5b@gmail.com>
2015-06-16 16:34:25 -0500

    Assignment 2, cachesolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrtx stores a matrix and the associated inverse
+## $get returns the original matrix
+## $set sets the matrix to the new input
+## $setinverse sets the inverse matrix to the input, no validation is done
+## $getinverse returns the contents of the cached inverse
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(x = matrix()) 
+{
+	m <- NULL
+  	set <- function(y) 
+  	{
+    	x <<- y
+    	m <<- NULL
+  	}
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+##cacheSolve attempts to find the inverse of a given matrix and 
+##load it back to the makeCacheMatrix 
+##A matrix must be square and nonsingular
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+    ## Return a matrix that is the inverse of 'x'
+	m <- x$getinverse()
+  	if(!is.null(m)) 
+  	{
+    	message("getting cached data")
+    	return(m)
+  	}
+  	data <- x$get()
+  	m <- solve(data, ...)
+  	x$setinverse(m)
+  	m
 }
